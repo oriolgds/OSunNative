db9fa39853a66bc93c0e712a4c6bd43ac3fb7d24
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-09-11 14:40:40 +0200

    NativeBaseProvider installed

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {ImageBackground, ScrollView, StyleSheet, Text, View} from 'react-native';
+import { NativeBaseProvider, Box } from "native-base";
 
-export default function App() {
+const image = require('./assets/cloudy.jpg');
+
+const roundedContainerStyles = StyleSheet.create({
+  normal: {
+    flex: 1,
+    alignContent: 'start',
+    backgroundColor: '#fffa',
+    height: 'auto',
+    width: 'auto',
+    borderRadius: 30,
+    margin: 10,
+  
+  }
+});
+const RoundedContainer = ()=>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={roundedContainerStyles.normal}>
+      <Text style={{fontSize: 200}}>20º</Text>
     </View>
-  );
+  )
 }
 
+const App = () => (
+  <NativeBaseProvider>
+  <View style={styles.all}>
+    <ImageBackground source={image} resizeMode="cover" style={styles.backgroundImage}>
+    <ScrollView style={{marginTop: 30}}>
+    <RoundedContainer/>
+    </ScrollView>
+    </ImageBackground>
+  </View>
+  </NativeBaseProvider>
+);
+
 const styles = StyleSheet.create({
-  container: {
+  all: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
     justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
 });
+
+export default App;
