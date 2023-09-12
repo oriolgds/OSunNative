47b20c3f29cf9bc2f2ff47af2a08d5d39c22b514
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-09-12 16:06:06 +0200

    Apple design

--- App.js
+++ App.js
@@ -1,35 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {ImageBackground, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {ImageBackground, ScrollView, StyleSheet, Text, View, Dimensions} from 'react-native';
+
 
 
 const image = require('./assets/cloudy.jpg');
 
+function marginToMaxWidth(width = 330){
+  return Dimensions.get('window').width < width ? 0 : Dimensions.get('window').width - width
+}
 
 const roundedContainerStyles = StyleSheet.create({
   normal: {
     flex: 1,
-    alignContent: 'start',
+    alignContent: 'center',
+    justifyContent: 'center',
     backgroundColor: '#fffa',
     height: 'auto',
     width: 'auto',
     borderRadius: 30,
     margin: 10,
-  
+    marginHorizontal: marginToMaxWidth(360),
+    textAlign: 'center',
   }
 });
-const RoundedContainer = ()=>{
-  return (
-    <View style={roundedContainerStyles.normal}>
-      <Text style={{fontSize: 200, textAlign: 'center'}}>21º</Text>
-    </View>
-  )
+class RoundedContainer extends React.Component{
+  render(){
+    return (
+      <View style={roundedContainerStyles.normal}>
+          {this.props.children}
+      </View>
+    )
+  }
 }
 
 const App = () => (
   <View style={styles.all}>
+    <StatusBar backgroundColor='#fff'></StatusBar>
     <ImageBackground source={image} resizeMode="cover" style={styles.backgroundImage}>
     <ScrollView style={{marginTop: 30}}>
-    <RoundedContainer/>
+      <RoundedContainer>
+        <Text style={{fontSize: 200, textAlign: 'center'}}>21º</Text>
+      </RoundedContainer>
+
+      <RoundedContainer>
+        <Text style={{fontSize: 200, textAlign: 'center'}}>21°</Text>
+      
+      </RoundedContainer>
     </ScrollView>
     </ImageBackground>
   </View>
